24e9ef21480f2b7e94ff7e683cffd051b19036bd
Dustin Moris Gorski <dustinmoris@gmail.com>
2016-09-12 08:30:10 +0100

    work in progress

--- src/Suave.AspNetCore/HttpContextExtensions.cs
+++ src/Suave.AspNetCore/HttpContextExtensions.cs
@@ -18,22 +18,39 @@ namespace Suave.AspNetCore
                     context.Request.Headers
                         .Select(h => new Tuple<string, string>(h.Key, h.Value))
                         .ToList());
-
+            
             return Http.HttpContextModule.create(
                 new Http.HttpRequest(
-                    "HTTP VERSION",
+                    context.Request.Protocol,
                     new Uri(""),
-                    "host",
-                    Http.HttpMethod.GET,
+                    context.Request.Host.Value,
+                    HttpMethodFromString(context.Request.Method),
                     headers,
-                    null,
+                    null, // byte[] rawForm
                     "rawQuery",
-                    null,
-                    null,
-                    null),
-                null,
-                null,
-                false);
+                    null, // FSharpList<HttpUpload> files,
+                    null, // FSharpList<Tuple<string,string>> multiPartFields
+                    null), // TraceHeader trace
+                null, // HttpRuntime
+                null , // Connection
+                false); // bool writePreamble
+        }
+
+        public static Http.HttpMethod HttpMethodFromString(string method)
+        {
+            switch (method)
+            {
+                case "GET": return Http.HttpMethod.GET;
+                case "POST": return Http.HttpMethod.POST;
+                case "PUT": return Http.HttpMethod.PUT;
+                case "DELETE": return Http.HttpMethod.DELETE;
+                case "PATCH": return Http.HttpMethod.PATCH;
+                case "CONNECT": return Http.HttpMethod.CONNECT;
+                case "HEAD": return Http.HttpMethod.HEAD;
+                case "OPTIONS": return Http.HttpMethod.OPTIONS;
+                case "TRACE": return Http.HttpMethod.TRACE;
+                default: throw new InvalidOperationException($"{method} is not a valid HTTP verb.");
+            }
         }
 
         public static async Task SetResponseFromSuaveHttpContext(this HttpContext context, Http.HttpContext suaveContext)
