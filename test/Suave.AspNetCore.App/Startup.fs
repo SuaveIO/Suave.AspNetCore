82b85f3094f1a1a1e5745afaa243d6f0b3e432bc
Dustin Moris Gorski <dustinmoris@gmail.com>
2016-12-18 22:55:11 +0000

    testing some stuff

--- test/Suave.AspNetCore.App/Startup.fs
+++ test/Suave.AspNetCore.App/Startup.fs
@@ -7,20 +7,49 @@ open Microsoft.Extensions.DependencyInjection
 open Microsoft.Extensions.Logging
 open Newtonsoft.Json
 open Suave
+open Suave.Filters
 open Suave.Operators
+open Suave.DotLiquid
 open Suave.Successful
 open Suave.AspNetCore
 
 module App =
+    //
+    // DotLiquid
+    //
+    type ViewModel = 
+        {
+            Message : string
+        }   
+
+    DotLiquid.setTemplatesDir (__SOURCE_DIRECTORY__ + "./Views")
+
+    let dotLiquidHandler = 
+        path "/dotliquid" 
+        >=> DotLiquid.page "index.html" { Message = "Hello World from a DotLiquid template." }
+
+    //
+    // Everything else
+    //
     let catchAll =
         fun (ctx : HttpContext) ->
             let json = JsonConvert.SerializeObject(ctx.request, Formatting.Indented)
             OK json 
             >=> Writers.setMimeType "application/json"
             <| ctx
+    
+    
+    //
+    // App
+    //
+    let app =
+        choose [
+            dotLiquidHandler
+            catchAll
+            ]
 
 type Startup() =
     member __.Configure (app : IApplicationBuilder)
                         (env : IHostingEnvironment)
                         (loggerFactory : ILoggerFactory) =
-        app.UseSuave(App.catchAll) |> ignore
+        app.UseSuave(App.app) |> ignore
--- test/Suave.Vanilla.App/Program.fs
+++ test/Suave.Vanilla.App/Program.fs
@@ -1,9 +1,52 @@
 ﻿open System
 open Suave
+open Suave.Filters
 open Suave.Successful
 open Suave.Operators
+open Suave.DotLiquid
+open Suave.Sockets
+open Suave.Sockets.Control
+open Suave.WebSocket
 open Newtonsoft.Json
 
+//
+// DotLiquid
+//
+type ViewModel = 
+    {
+        Message : string
+    }   
+
+DotLiquid.setTemplatesDir (__SOURCE_DIRECTORY__ + "./Views")
+
+let dotLiquidHandler = 
+    path "/dotliquid" 
+    >=> DotLiquid.page "index.html" { Message = "Hello World from a DotLiquid template." }
+
+//
+// WebSockets
+//
+let echo (webSocket : WebSocket) =
+    fun ctx ->
+        socket {
+            let loop = ref true
+            while !loop do
+                let! msg = webSocket.read()
+                match msg with
+                | (Text, data, true) ->
+                    let str = UTF8.toString data
+                    do! webSocket.send Text (ArraySegment data) true
+                | (Ping, _, _) ->
+                    do! webSocket.send Pong (ArraySegment([||])) true
+                | (Close, _, _) ->
+                    do! webSocket.send Close (ArraySegment([||])) true
+                    loop := false
+                | _ -> ()
+        }
+
+//
+// Everything else
+//
 let catchAll =
     fun (ctx : HttpContext) ->
         let json = JsonConvert.SerializeObject(ctx.request, Formatting.Indented)
@@ -11,7 +54,17 @@ let catchAll =
         >=> Writers.setMimeType "application/json"
         <| ctx
 
+//
+// App
+//
+let app =
+    choose [
+        path "/websocket" >=> handShake echo
+        dotLiquidHandler
+        catchAll
+        ]
+
 [<EntryPoint>]
 let main argv = 
-    startWebServer defaultConfig catchAll
+    startWebServer defaultConfig app
     0
